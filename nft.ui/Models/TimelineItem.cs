962cdda27c50c380ea31627990804249f1b5cfbd
Nico Pizzo <funkiernails@yahoo.com>
2022-01-16 13:12:45 -0500

    updated timeline

---
+++ nft.ui/Models/TimelineItem.cs
@@ -0,0 +1,9 @@
+﻿namespace nft.ui.Models
+{
+    public class TimelineItem
+    {
+        public string Name { get; set; }
+        public string Achievement { get; set; }
+        public string Description { get; set; }
+    }
+}
