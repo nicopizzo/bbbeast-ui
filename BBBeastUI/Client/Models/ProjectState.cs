4eefb46ea2465a07340a04804e24d6266ba2c47b
Nico Pizzo <funkiernails@yahoo.com>
2022-01-29 17:37:59 -0500

    added faq and refactor

--- BBBeastUI/Client/Models/ProjectState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BBBeastUI.Models
-{
-    public class ProjectState
-    {
-        public bool PrivateSaleLive { get; set; }
-        public bool PublicSaleLive { get; set; }
-        public bool SoldOut { get; set; }
-    }
-}
