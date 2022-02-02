5323b6fe9670c7ee2aa581fb10f08d43c38dba6e
Nico Pizzo <funkiernails@yahoo.com>
2022-02-01 21:33:44 -0500

    fixed footer

---
+++ BBBeastUI/Client/Models/Links.cs
@@ -0,0 +1,8 @@
+﻿namespace BBBeastUI.Models
+{
+    public static class Links
+    {
+        public const string DISCORD = "https://discord.com";
+        public const string TWITTER = "https://twitter.com";
+    }
+}
